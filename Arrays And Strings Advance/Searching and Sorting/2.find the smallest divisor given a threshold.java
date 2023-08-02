e7f8c3d57b19911dee1d7a8b16f5de38e7647088
Sudhanshu Mishra <mishrasudhanshu855@gmail.com>
2023-08-03 00:28:23 +0530

    day9 :bulb:

--- Arrays And Strings Advance/Searching and Sorting/2.find the smallest divisor given a threshold.java
+++ Arrays And Strings Advance/Searching and Sorting/2.find the smallest divisor given a threshold.java
@@ -7,7 +7,7 @@ Find the smallest divisor such that the result mentioned above is less than or e
 
 /*
 Approach :
-1.lo at 1 and hi at constraint i.e 10000000
+1.lo at 1 and hi at maximum value in array
 2.calculate mid 
 3.us mid ko divsor man ke calculate sum of quotients for each element in array
 4.if sum > threshold -> mid is smaller i.e ans is b/w mid + 1 to hi so lo= mid +1
@@ -21,7 +21,6 @@ div-3 = 1+1+2+3 =  7>6
 div-4= 1+1+2 +3 =  7>6
 div-5 = 1+1+1+2 = 6 = 6
 
-const - divisor 10 ki power 6 se bda nhi hoga isko hi set kr dege
 mid nikal ke sum nikalege agar ye threshold se badi aata h toh mid chota le liya bada number lena tha toh 
 mid + 1 to hi me hamra answer h else me mid bda le liya h mid se lo tak dekhege
 max se bde and min se chote se divide nhi kr skte -> no effect
@@ -30,20 +29,20 @@ max se bde and min se chote se divide nhi kr skte -> no effect
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
         int lo = 1;
-        int hi = 1000000;
+        int hi = Integer.MIN_VALUE;
+        for(int val : nums){
+            hi = Math.max(hi,val);
+        }
         while(lo < hi){
             int mid = (lo + hi)/2;
             int sum = 0;
             for(int val : nums){
-                //mid se divide krke calculating quo and taking sum
                 int quo = (val + mid -1) / mid;
                 sum += quo;
             }
             if(sum > threshold){
-                //sum bda h i.e divsor chota h
                 lo = mid + 1;
             } else {
-                //sum kam h i.e divisor bda h
                 hi = mid;
             }
         }
